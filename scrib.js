27dd94928bd74c16779f56039d736d64605807d9
helmybahtiyar93 <76771096+helmybahtiyar93@users.noreply.github.com>
2020-12-30 23:43:59 +0700

    Add files via upload

---
+++ scrib.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function(){
+    const penjelasan = document.querySelector('.penjelasan');
+    const gambar = document.querySelector('.gambar');
+
+
+    penjelasan.classList.add('muncul1');
+
+    gambar.classList.add('muncul2');
+})
